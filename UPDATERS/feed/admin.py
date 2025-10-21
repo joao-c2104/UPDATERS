61ed4d6ceb39ae48833575054b072a68839f1ef7
João Carlos <joaocarlosv.2104@gmail.com>
2025-10-21 20:19:11 -0300

    noticias

--- UPDATERS/feed/admin.py
+++ UPDATERS/feed/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Article
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'publication_date')
-    search_fields = ('title', 'content')
+    list_display = ('title', 'author', 'publication_date')
+    search_fields = ('title', 'content', 'author')
 
 admin.site.register(Article, ArticleAdmin)
