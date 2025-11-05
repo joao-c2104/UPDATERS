76533df2ccc9974cef5e78e8790d66772c9a81b5
Walter Maia <waltersantiagoneto@gmail.com>
2025-11-05 01:00:33 -0300

    Adicionando path urls em apps e configurando o rest_framework

--- backend/news/views.py
+++ backend/news/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Article
+from .serializers import ArticleSerializer
 
-# Create your views here.
+class ArticleViewSet(viewsets.ModelViewSet):
+    queryset = Article.objects.all().order_by("-published_at")
+    serializer_class = ArticleSerializer
+
