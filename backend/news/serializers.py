c1c39efd7cd92175d9f586f4aed03f93323bb855
Walter Maia <waltersantiagoneto@gmail.com>
2025-11-05 01:53:56 -0300

    Adicionando comentários, video e enquete models e  APIs

--- backend/news/serializers.py
+++ backend/news/serializers.py
@@ -1,8 +1,26 @@
 from rest_framework import serializers
-from .models import Article
+from .models import Article, Comment, Video, Poll
 
 
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
+        fields = "__all__"
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = "__all__"
+
+
+class VideoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Video
+        fields = "__all__"
+
+
+class PollSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Poll
         fields = "__all__"
