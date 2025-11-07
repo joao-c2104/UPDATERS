72b38b811537c11ec51e76045eabedf59a0cdbb4
Saturnacy <jppessoaprimo@gmail.com>
2025-11-06 21:12:06 -0300

    Consertado errode referência no login

--- main/feed/urls.py
+++ main/feed/urls.py
@@ -2,17 +2,9 @@ from django.urls import path
 from .views import (
     ArticleListView, 
     ArticleDetailView,
-    login_user,
-    submit_login,
-    register_user,
-    logout_user
 )
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='article-list'),
     path('article/<slug:slug>/', ArticleDetailView.as_view(), name='article-detail'),
-    path('login/', login_user, name='login_user'), 
-    path('login/submit/', submit_login, name='submit_login'),
-    path('register/', register_user, name='register_user'),
-    path('logout/', logout_user, name='logout_user'),
 ]
